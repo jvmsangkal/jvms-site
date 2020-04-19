8ab76ddd085397144eaa13b58bf8bc135d3d9b58
John Viscel Sangkal <jvmsangkal@gmail.com>
2020-04-19 19:43:50 +0800

    attempt to fix service-worker

--- next.config.js
+++ next.config.js
@@ -18,6 +18,34 @@ const nextConfig = {
 
     return config
   },
+
+  workboxOpts: {
+    swDest: process.env.NEXT_EXPORT
+      ? 'service-worker.js'
+      : 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'offlineCache',
+          expiration: {
+            maxEntries: 200,
+          },
+        },
+      },
+    ],
+  },
+  experimental: {
+    async rewrites() {
+      return [
+        {
+          source: '/service-worker.js',
+          destination: '/_next/static/service-worker.js',
+        },
+      ]
+    },
+  },
 }
 
 module.exports = withOffline(nextConfig)
