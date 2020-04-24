70263c98245c594f2f59f12f736923a3e39cd665
John Viscel Sangkal <jvmsangkal@gmail.com>
2020-04-24 18:45:20 +0800

    implement work page

--- components/layout.js
+++ components/layout.js
@@ -1,35 +1,48 @@
 import Header from './header'
 import Footer from './footer'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 
 import { LazyLoadImage } from 'react-lazy-load-image-component'
 
 // How to implement layouts with Higher Order Components:
 // https://nextjs.org/learn/basics/using-shared-components/rendering-children-components
 
-const Layout = ({ children, home }) => (
-  <div className="min-h-screen flex flex-wrap justify-center">
-    {home ? (
-      <div className="lg:w-1/2 sm:w-full">
-        <LazyLoadImage
-          wrapperClassName="block lg:h-full w-full h-64"
-          className="lg:h-full w-full h-64 object-cover object-center"
-          alt="John Viscel"
-          effect="blur"
-          placeholderSrc={require('../assets/images/background.jpg?lqip')}
-          src={require('../assets/images/background.jpg?resize&size=1500')}
-        />
+const Layout = ({ children, home }) => {
+  const contentDivClass = classNames(
+    'p-6',
+    'md:px-16',
+    'md:py-8',
+    'w-full',
+    'flex',
+    'flex-col',
+    { 'lg:w-1/2': home, 'lg:w-2/3': !home }
+  )
+
+  return (
+    <div className="min-h-screen flex flex-wrap justify-center">
+      {home ? (
+        <div className="lg:w-1/2 sm:w-full">
+          <LazyLoadImage
+            wrapperClassName="block lg:h-full w-full h-64"
+            className="lg:h-full w-full h-64 object-cover object-center"
+            alt="John Viscel"
+            effect="blur"
+            placeholderSrc={require('../assets/images/background.jpg?lqip')}
+            src={require('../assets/images/background.jpg?resize&size=1500')}
+          />
+        </div>
+      ) : (
+        ''
+      )}
+      <div className={contentDivClass}>
+        <Header />
+        <main className="flex-grow">{children}</main>
+        <Footer />
       </div>
-    ) : (
-      ''
-    )}
-    <div className="p-6 md:px-16 md:py-8 lg:w-1/2 w-full flex flex-col">
-      <Header />
-      <main className="flex-grow">{children}</main>
-      <Footer />
     </div>
-  </div>
-)
+  )
+}
 
 Layout.propTypes = {
   children: PropTypes.node,
--- pages/work.js
+++ pages/work.js
@@ -1,13 +1,71 @@
 import { Helmet } from 'react-helmet'
 import Layout from '../components/layout'
+import PropTypes from 'proptypes'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
 
-export default function About() {
+const works = [
+  {
+    link: 'https://www.importgenius.com/',
+    title: 'Importgenius Website',
+    lqImageSrc: require('../assets/images/ig-site.png?lqip'),
+    imageSrc: require('../assets/images/ig-site.png?webp'),
+    shortDesc:
+      'Comprehensive trade databases from ImportGenius support import-export business in finding new global opportunities, research markets, evaluate trade partners, and more.',
+  },
+  {
+    link: 'https://next.importgenius.com/',
+    title: 'Importgenius App Next-Gen',
+    lqImageSrc: require('../assets/images/isng.png?lqip'),
+    imageSrc: require('../assets/images/isng.png?webp'),
+    shortDesc: 'Beta release of the new ImportGenius App',
+  },
+  {
+    link: 'https://www.tunes.tm/#/',
+    title: 'Tunes.tm',
+    lqImageSrc: require('../assets/images/tunes.png?lqip'),
+    imageSrc: require('../assets/images/tunes.png?webp'),
+    shortDesc:
+      'Tunes is a Music Discovery Service that allows Music Artists or Music Providers to import their music into Tunes for others to discover.',
+  },
+]
+
+function WorkCard({ link, title, lqImageSrc, imageSrc, shortDesc }) {
+  return (
+    <div className="mt-4 mb-8">
+      <a className="hover:text-blue-700 hover:underline" href={link}>
+        <LazyLoadImage
+          wrapperClassName="w-full max-w-xl rounded-md shadow-lg object-cover object-center"
+          className="w-full max-w-xl rounded-md shadow-lg object-cover object-center"
+          alt={title}
+          effect="blur"
+          placeholderSrc={lqImageSrc}
+          src={imageSrc}
+        />
+        <h2 className="text-lg font-bold tracking-wide">{title}</h2>
+      </a>
+      <p className="text-gray-600 mb-4">{shortDesc}</p>
+      <hr />
+    </div>
+  )
+}
+
+WorkCard.propTypes = {
+  link: PropTypes.string,
+  title: PropTypes.string,
+  lqImageSrc: PropTypes.object,
+  imageSrc: PropTypes.string,
+  shortDesc: PropTypes.string,
+}
+
+export default function Work() {
   return (
     <Layout>
       <Helmet>
         <title>Work</title>
       </Helmet>
-      <p>Work in progress. Coming soon.</p>
+      {works.map((work) => (
+        <WorkCard {...work} key={work.title} />
+      ))}
     </Layout>
   )
 }
